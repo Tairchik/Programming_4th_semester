071ed79feebb37dbd941875e5431ad3ad408d9e2
Dmewtriy <dankin.dm@mail.ru>
2025-05-06 12:39:16 +0700

    Разработан сервер для 4 части лабы
    Программа берёт количество установок из файла конфигурации
    Генерирует состояния кнопок согласно количеству
    Начинает генерацию после ответа от клиента о готовности

---
+++ lab3/ServerTask4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+using System.Globalization;
+
+namespace ServerTask4
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string numberOfInstallationsString = ConfigurationManager.AppSettings["numberOfInstallations"];
+            int numberOfInstallations = int.Parse(numberOfInstallationsString);
+            ServerTask4 server = new ServerTask4(numberOfInstallations);
+            try
+            {
+                server.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                server.Stop();
+            }
+        }
+    }
+}
---
+++ lab3/ServerTask4/ServerTask4.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerTask4
+{
+    internal class ServerTask4
+    {
+        private readonly TcpListener _server;
+        private bool _isRunning;
+        private bool connected = false;
+        private readonly int _numberOfInstallations;
+        private string[] unitStates;
+        public ServerTask4(int numberOfInstallations)
+        {
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            _server = new TcpListener(localAddr, 8888);
+            _numberOfInstallations = numberOfInstallations;
+            unitStates = new string[numberOfInstallations];
+            Array.Fill(unitStates, "WORKING");
+        }
+
+        public void Start()
+        {
+            _server.Start();
+            _isRunning = true;
+            TcpClient client;
+            NetworkStream stream;
+            Random _random = new Random();
+
+            bool readyForGenerate = false;
+
+            while (_isRunning)
+            {
+                client = _server.AcceptTcpClient();
+                connected = true;
+                stream = client.GetStream();
+                DateTime dateTime = DateTime.Now;
+
+                // При установлении соединения передаем количество установок
+                SendNumberOfInstallations(stream);
+
+                while (connected)
+                {
+                    if ((DateTime.Now - dateTime).TotalSeconds < 2 && !readyForGenerate)
+                    {
+                        HandleClient(stream, out readyForGenerate);
+                    }
+                    else
+                    {
+                        dateTime = DateTime.Now;
+                        // Генерация значений
+                        GenerateValues();
+
+                        // Формирование строки данных
+                        string data = string.Join(";", unitStates);
+
+                        SendResponse(stream, data);
+                    }
+                }
+                client.Close();
+            }
+        }
+
+        private void GenerateValues()
+        {
+            Random rnd = new Random();
+            double stateProbability;
+            double repairProbability;
+            for (int i = 0; i < _numberOfInstallations; i++)
+            {
+                if (unitStates[i] == "WORKING")
+                {
+                    stateProbability = rnd.NextDouble();
+                    if (stateProbability >= 0.8)
+                    {
+                        unitStates[i] = "FAILURE"; // Переход в аварию
+                    }
+                }
+                else if (unitStates[i] == "FAILURE")
+                {
+                    unitStates[i] = "REPAIR"; // Переход в ремонт
+                }
+                else if (unitStates[i] == "REPAIR")
+                {
+                    // Проверка вероятности восстановления (50%)
+                    repairProbability = rnd.NextDouble();
+                    if (repairProbability < 0.5)
+                    {
+                        unitStates[i] = "WORKING"; // Возврат в рабочее состояние
+                    }
+                }
+            }
+        }
+
+        private void SendNumberOfInstallations(NetworkStream stream)
+        {
+            SendResponse(stream, _numberOfInstallations.ToString());
+        }
+
+        public void Stop()
+        {
+            _isRunning = false;
+            _server.Stop();
+        }
+
+        private void HandleClient(NetworkStream stream, out bool readyForGenerate)
+        {
+            byte[] buffer = new byte[20];
+            int bytesRead;
+            readyForGenerate = false;
+            if (stream.DataAvailable)
+            {
+                bytesRead = stream.Read(buffer, 0, buffer.Length);
+                if (bytesRead == 0)
+                {
+                    // Client disconnected
+                    return;
+                }
+
+                string request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                Console.WriteLine($"Получен запрос: {request}");
+
+                if (request == "?Disconnect")
+                {
+                    connected = false;
+                    return;
+                }
+                else if (request == "?Ready")
+                {
+                    readyForGenerate = true;
+                }
+            }
+            
+           
+
+        }
+
+        private void SendResponse(NetworkStream stream, string response)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(response);
+            stream.Write(data, 0, data.Length);
+            Console.WriteLine($"Отправлен ответ: {Encoding.UTF8.GetString(data)}");
+        }
+    }
+}
